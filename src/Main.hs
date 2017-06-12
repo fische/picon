28f7b039d96f6da6e8a96e24d3b7dcc7a729bc2b
Maxime Fischer <maxime2fischer@gmail.com>
2017-06-12 21:21:34 +0200

    Add python file parsing.

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,15 @@
 module Main where
 
+import qualified System.Environment as Env
+import qualified Language.Python.Common.Pretty as Pretty
+import qualified Language.Python.Common.PrettyParseError ()
+import qualified Language.Python.Common.PrettyAST ()
+import qualified Language.Python.Version2 as Python2
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  [file] <- Env.getArgs -- TODO Handle correctly args
+  code <- readFile file
+  case Python2.parseModule code file of
+       Left err -> putStrLn $ Pretty.prettyText err
+       Right (pymodule, _) -> putStrLn $ Pretty.prettyText pymodule
