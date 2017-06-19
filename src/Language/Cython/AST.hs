806652e7601b71ff7a24e4382b181503c9e8e2e4
Dominic Orchard <dom.orchard@gmail.com>
2017-06-19 14:16:23 +0100

    suggestion on how to make 'cythonize'

--- src/Language/Cython/AST.hs
+++ src/Language/Cython/AST.hs
@@ -1,14 +1,17 @@
+{-# LANGUAGE MultiParamTypeClasses #-}
+
 module Language.Cython.AST where
 
 import qualified Language.Python.Common.AST as AST
 import Language.Python.Common.SrcLocation (Span)
 import Data.Data
 
-class Cythonizable p where
+class Cythonizable p c where
+  -- Convert Python AST (from Language.Python.Common.AST) to the AST here
   cythonize :: p -> c
 
 newtype Module annot = Module [AST.Statement annot]
   deriving (Eq,Ord,Show,Typeable)
 
-instance Cythonizable (AST.Module annot) where
+instance Cythonizable (AST.Module annot) (Module annot) where
   cythonize (AST.Module stmts) = Module stmts
