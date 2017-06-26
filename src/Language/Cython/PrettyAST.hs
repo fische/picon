e4782a5d08839f1174457e75927900daf1fceeb4
Maxime Fischer <maxime2fischer@gmail.com>
2017-06-26 17:50:46 +0200

    Remove useless import.

--- src/Language/Cython/PrettyAST.hs
+++ src/Language/Cython/PrettyAST.hs
@@ -5,7 +5,6 @@ module Language.Cython.PrettyAST where
 import qualified Language.Python.Common.AST as AST
 import Language.Cython.AST
 import Language.Python.Common.Pretty
-import Language.Python.Common.SrcLocation (Span)
 import Language.Python.Common.PrettyAST ()
 
 instance {-# OVERLAPS #-} Pretty (AST.Statement (Annotation, a)) where
