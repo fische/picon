4945a1a2f2d4ad9815145e9703ed062b96a88af1
Maxime Fischer <maxime2fischer@gmail.com>
2017-07-23 15:25:19 +0200

    Print tree with annotations from analysis.

--- src/Main.hs
+++ src/Main.hs
@@ -5,12 +5,8 @@ import qualified System.Environment as Env (getArgs)
 import qualified Language.Python.Version2 as Python2 (parseModule)
 import Language.Python.Common.Pretty (prettyText)
 import Language.Python.Common.PrettyParseError ()
-
-import Language.Cython.PrettyAST ()
-import Language.Cython.AST
 import Language.Cython.Context
-import Control.Monad.State
-import Control.Monad.Trans.Except
+import Language.Cython
 
 main :: IO ()
 main = do
@@ -19,7 +15,5 @@ main = do
   case Python2.parseModule code file of
       Left err -> putStrLn $ prettyText err
       Right (pymodule, _) ->
-        let tree = initCythonAST pymodule
-            ctx = emptyContext
-            results = evalState (runExceptT $ cythonize tree) ctx
-        in putStrLn $ either prettyText prettyText results
+        let ctx = empty
+        in either print print $ cythonize ctx pymodule
