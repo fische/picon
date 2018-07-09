d28cfb6d7374de3ea06d7eafc772efa24671ed32
Maxime Fischer <maxime2fischer@gmail.com>
2018-07-09 09:28:13 +0100

    Add functions to find a specific node in a tree.

--- src/Data/Tree/Path.hs
+++ src/Data/Tree/Path.hs
@@ -2,11 +2,20 @@ module Data.Tree.Path (
   Path(..),
   update,
   updateM,
-  get
+  get,
+  findl,
+  findr,
+  findlWithPath,
+  findrWithPath,
+  findlM,
+  findrM,
+  findlWithPathM,
+  findrWithPathM
 ) where
 
 import           Data.Semigroup
 import           Data.Tree
+import           Data.Maybe
 
 newtype Path = Path [Int]
   deriving (Eq,Ord,Show)
@@ -32,6 +41,76 @@ updateM f (Path (idx:l)) t = do
 get :: Path -> Tree a -> Tree a
 get (Path []) t      = t
 get (Path (idx:l)) t = get (Path l) $ subForest t !! idx
+
+findl :: (a -> Maybe b) -> Path -> Tree a -> Maybe b
+findl f (Path []) t = f $ rootLabel t
+findl f (Path (idx:l)) t
+  | isNothing found = findl f (Path l) $ subForest t !! idx
+  | otherwise = found
+  where found = f $ rootLabel t
+
+findr :: (a -> Maybe b) -> Path -> Tree a -> Maybe b
+findr f (Path []) t = f $ rootLabel t
+findr f (Path (idx:l)) t
+  | isNothing found = f $ rootLabel t
+  | otherwise = found
+  where found = findr f (Path l) $ subForest t !! idx
+
+findlWithPath' :: (Path -> a -> Maybe b) -> Path -> Path -> Tree a -> Maybe b
+findlWithPath' f pos (Path []) t = f pos $ rootLabel t
+findlWithPath' f pos (Path (idx:l)) t
+  | isNothing found = findlWithPath' f (pos <> Path [idx]) (Path l) $ subForest t !! idx
+  | otherwise = found
+  where found = f pos $ rootLabel t
+
+findlWithPath :: (Path -> a -> Maybe b) -> Path -> Tree a -> Maybe b
+findlWithPath f p@(Path (idx:_)) = findlWithPath' f (Path [idx]) p
+findlWithPath f p = findlWithPath' f p p
+
+findrWithPath' :: (Path -> a -> Maybe b) -> Path -> Path -> Tree a -> Maybe b
+findrWithPath' f pos (Path []) t = f pos $ rootLabel t
+findrWithPath' f pos (Path (idx:l)) t
+  | isNothing found = f pos $ rootLabel t
+  | otherwise = found
+  where found = findrWithPath' f (pos <> Path [idx]) (Path l) $ subForest t !! idx
+
+findrWithPath :: (Path -> a -> Maybe b) -> Path -> Tree a -> Maybe b
+findrWithPath f p@(Path (idx:_)) = findrWithPath' f (Path [idx]) p
+findrWithPath f p = findrWithPath' f p p
+
+findlM :: (Monad m) => (a -> m (Maybe b)) -> Path -> Tree a -> m (Maybe b)
+findlM f (Path []) t = f $ rootLabel t
+findlM f (Path (idx:l)) t = do
+  found <- f $ rootLabel t
+  let notFound = findlM f (Path l) $ subForest t !! idx
+  maybe notFound (return . Just) found
+
+findrM :: (Monad m) => (a -> m (Maybe b)) -> Path -> Tree a -> m (Maybe b)
+findrM f (Path []) t = f $ rootLabel t
+findrM f (Path (idx:l)) t = do
+  found <- findrM f (Path l) $ subForest t !! idx
+  maybe (f $ rootLabel t) (return . Just) found
+
+findlWithPathM' :: (Monad m) => (Path -> a -> m (Maybe b)) -> Path -> Path -> Tree a -> m (Maybe b)
+findlWithPathM' f pos (Path []) t = f pos $ rootLabel t
+findlWithPathM' f pos (Path (idx:l)) t = do
+  found <- f pos $ rootLabel t
+  let notFound = findlWithPathM' f (pos <> Path [idx]) (Path l) $ subForest t !! idx
+  maybe notFound (return . Just) found
+
+findlWithPathM :: (Monad m) => (Path -> a -> m (Maybe b)) -> Path -> Tree a -> m (Maybe b)
+findlWithPathM f p@(Path (idx:_)) = findlWithPathM' f (Path [idx]) p
+findlWithPathM f p = findlWithPathM' f p p
+
+findrWithPathM' :: (Monad m) => (Path -> a -> m (Maybe b)) -> Path -> Path -> Tree a -> m (Maybe b)
+findrWithPathM' f pos (Path []) t = f pos $ rootLabel t
+findrWithPathM' f pos (Path (idx:l)) t = do
+  found <- findrWithPathM' f (pos <> Path [idx]) (Path l) $ subForest t !! idx
+  maybe (f pos $ rootLabel t) (return . Just) found
+
+findrWithPathM :: (Monad m) => (Path -> a -> m (Maybe b)) -> Path -> Tree a -> m (Maybe b)
+findrWithPathM f p@(Path (idx:_)) = findrWithPathM' f (Path [idx]) p
+findrWithPathM f p = findrWithPathM' f p p
 
 instance Monoid Path where
   mempty = Path []
